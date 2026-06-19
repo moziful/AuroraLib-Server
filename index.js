144a5ffa3bc61e49dabb5672c311c360a909c44a
Moziful Haque <mhmoni2310@gmail.com>
2026-06-19 23:48:47 +0600

    Trying to solve deployment error

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
         //Here we will get data.
         const db = client.db('AuroraLib');
@@ -32,7 +32,7 @@ async function run() {
         });
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
