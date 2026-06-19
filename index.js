98c7b83ad7ddb47db8e51eb29585094c031d387f
Moziful Haque <mhmoni2310@gmail.com>
2026-06-20 05:39:33 +0600

    Trying CORS issue fixed

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ const verifyToken = (req, res, next) => {
 };
 
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: process.env.CLIENT_URL || "http://localhost:3000",
     credentials: true,
 }));
 app.use(express.json());
